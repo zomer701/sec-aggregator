056d2932d2c3a6f7b6ecc127a952372cf098ec42
Mykhailo Markhain <mmarkhain@totalwine.com>
2024-05-12 21:21:34 -0400

    clean aggregator

--- src/main/java/io/scommerce/core/inbound/aggregator/bucket/BucketClient.java
+++
@@ -1,9 +0,0 @@
-package io.scommerce.core.inbound.aggregator.bucket;
-
-import io.scommerce.core.inbound.shared.constants.Constants;
-import org.springframework.http.codec.multipart.FilePart;
-import reactor.core.publisher.Mono;
-
-public interface BucketClient {
-    Mono<Void> upload(FilePart file,  Constants.ProcessorChannels channel);
-}
